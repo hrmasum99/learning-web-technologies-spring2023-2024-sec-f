1297c5cef82bbfd9fc39822bff78709d7ef6eb29
Habibur Rahman Masum <127416864+hrmasum99@users.noreply.github.com>
2024-03-04 23:39:57 +0600

    Add files via upload

---
+++ Lab 5.2/forgotPassword.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Forgot Password</title>
+    <style>
+        /* Style the fieldset */
+        fieldset {
+            width: 400px; /* Define width */
+            margin: 0 auto; /* Center horizontally */
+            text-align: left; /* Align text left */
+        }
+        /* Style the HR element */
+        hr {
+            width: 100%; /* Set width to fill the container */
+            margin: 10px 0; /* Add margin for spacing */
+        }
+    </style>
+</head>
+<body>
+    <table style="width: 720px;" border="1">
+        <tr>
+            <td>
+                &emsp;<b> X Company &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;   
+                <a href="home.php">Home</a> |
+                <a href="registration.php">Registration</a> |
+                <a href="login.php">Login</a>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">
+                <fieldset>
+                    <legend>Forgot Password</legend>
+                    Email:<input type="email" id="email" name="email"><br>
+                    <hr>
+                    <button type="submit">Submit</button>
+                </fieldset>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">
+                 Copyright &copy; 2024
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
